cc9cd6ea719bf8e4b048f5d5b8395c27892f2770
derL1onel <90069429+wasgitliom@users.noreply.github.com>
2022-12-11 18:13:16 +0100

    Add files via upload

---
+++ ContactFormWebsite/ContactForm.py
@@ -0,0 +1,40 @@
+# --- import
+import streamlit as st
+
+st.set_page_config(
+        page_title="Contact",
+        page_icon="mailbox_closed"
+        )
+
+
+
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
+
+
+# ---- CONTACT ----
+with st.container():
+    st.header("Contact")    
+    st.subheader("Get In Touch With Me!")
+    st.write("##")
+
+    # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
+    contact_form = """
+    <form action="https://formsubmit.co/liom1990moil@gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
